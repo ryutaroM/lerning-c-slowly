757055327604d1f94ba8235ecbc8ad0c67ca5efb
ryutaroM <arcticmonkey326@gmail.com>
2026-06-17 00:07:52 +0900

    chore: add .vscode and clang version properties

--- hello.c
+++ hello.c
@@ -2,6 +2,22 @@
 
 int main()
 {
-    puts("Apple");
-    puts("Banana");
+    printf("商品の価格を入力して下さい> \n");
+    int price;
+    scanf("%d", &price);
+
+    printf("支払った金額を入力して下さい> \n");
+    int payment;
+    scanf("%d", &payment);
+
+    int change = payment - price;
+    if (change < 0)
+    {
+        printf("支払った金額が足りません。\n");
+    }
+    else
+    {
+        printf("お釣りは%d円です。\n", change);
+    }
+    bool b = true;
 }
