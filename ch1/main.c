38c4150e4b7d841ced13b9b8c1f24d322baf7dc9
ryutaroM <arcticmonkey326@gmail.com>
2026-06-19 00:21:15 +0900

    feat: add error.c file and update main.c to display monthly names

--- ch1/main.c
+++ ch1/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    puts("Apple");
-    return 0;
+    puts("1\tJanuary");
+    puts("2\tFebruary");
+    puts("3\tMarch");
+    puts("12\tDecember");
+    puts("She found the \"key\"!");
+    puts("/(^_^)\\");
+    puts("/(^_^)\\n\\(^_^)/");
+    puts("|0\t|0\t");
+    puts("|1\t|1\t");
+    puts("|2\t|2\t");
+    puts("|12\t|12\t");
 }
