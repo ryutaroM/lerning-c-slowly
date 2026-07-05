f48f574f81194b3a58c29e95a26df2724f069690
ryutaroM <arcticmonkey326@gmail.com>
2026-07-05 13:02:26 +0900

    feat: add additional print statements for various calculations in calc.c

--- ch2/calc.c
+++ ch2/calc.c
@@ -22,4 +22,36 @@ int main()
     printf("%d\n", 10 / 5 * 2);
     printf("%d\n", 10 / 3);
     printf("%d\n", 10 % 3);
+
+    printf("%d\n", 10 % 3);
+    printf("%d\n", -10 % 3);
+
+    printf("%d時間%d分\n", 190 / 60, 190 % 60);
+
+    printf("千の位%d\n", 3456 / 1000 % 10);
+    printf("百の位%d\n", 3456 / 100 % 10);
+    printf("十の位%d\n", 3456 / 10 % 10);
+    printf("一の位%d\n", 3456 % 10);
+
+    printf("%d\n", 10 + 2 * 2);
+    printf("%d\n", (10 + 2) * 2);
+    printf("%d\n", (2 * (1 + 1) + 2) * 2);
+
+    printf("%d %d\n", 10 + 2 * 2, 5 + 8 + 7);
+    printf("%d %d\n", (10 + 2 * 2), (5 + 8 + 7));
+
+    // comment
+
+    /* comment */
+
+    /*
+    comment
+    */
+
+    /*
+    // nested comment
+    */
+
+    puts("/*string*/");
+    puts("//string");
 }
